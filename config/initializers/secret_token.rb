725225a8db62af06ca6b8fb68c5115cdd5c5b2a8
Frank Kotsianas <frankkotsianas@gmail.com>
2012-10-03 18:31:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skimmatic2::Application.config.secret_token = 'cbb64f0b78df5f9fe8637b9f468ca5bc660a9f1c695537f30c8961521422154915d2f4924f9a63291c904588dcdbf1b9b515b886310936394c1e80b184c807d8'
